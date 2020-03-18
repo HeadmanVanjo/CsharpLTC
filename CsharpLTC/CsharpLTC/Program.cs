1bb00635449a6759f80ad3f8078212fcf44fed2e
Sethmichael01 <49845849+Sethmichael01@users.noreply.github.com>
2020-03-18 23:47:39 +0100

    First Commit

---
+++ CsharpLTC/CsharpLTC/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: sethm
+ * Date: 18 Mar 2020
+ * Time: 9:54 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace CsharpLTC
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
